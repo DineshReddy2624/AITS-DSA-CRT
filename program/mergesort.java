d5b10fb6fe81fc1a00d14b806f5043f9312733ec
Dinesh Reddy <166133707+DineshReddy2624@users.noreply.github.com>
2025-06-10 15:34:38 +0530

    Create mergesort.java

---
+++ program/mergesort.java
@@ -0,0 +1,63 @@
+class mergesort
+{
+    public static void main(String[] args) 
+    {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        int low = 0;
+        int high = arr.length - 1;
+        mergeSort(arr, low, high);
+        for (int i = 0; i < arr.length; i++) 
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) 
+    {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[low + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = low;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
